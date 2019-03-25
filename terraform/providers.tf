bfafeb29b00d9ddeff2e14d01ed11859c9f3f258
Vilmos Nebehaj <vilmos@elotl.co>
2019-03-25 14:03:02 -0700

    Add envvars to providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,14 @@
 #
 
 provider "aws" {
-  region = "us-east-1"
+# You can also use environment variables instead of setting these variables:
+# $ export AWS_ACCESS_KEY_ID="anaccesskey"
+# $ export AWS_SECRET_ACCESS_KEY="asecretkey"
+# $ export AWS_DEFAULT_REGION="us-east-1"
+# See https://www.terraform.io/docs/providers/aws/ for more information.
+#  region = "us-east-1"
+#  access_key = ""
+#  secret_key = ""
 }
 
 # Using these data sources allows the configuration to be
