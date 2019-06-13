67d77d3a7ddbff6c1f13c86ba25de18b22f83ff2
Vilmos Nebehaj <vilmos@elotl.co>
2019-06-13 14:28:56 -0700

    Use --service-cluster-ip-range instead of --non-masquerade-cidr

--- terraform/eks-worker-nodes.tf
+++ terraform/eks-worker-nodes.tf
@@ -239,7 +239,7 @@ curl -L -O https://download.elotl.co/milpa-installer-latest && chmod 755 milpa-i
 ./milpa-installer-latest
 pip install yq
 yq -y ".clusterName=\"${var.cluster-name}\" | .cloud.aws.accessKeyID=\"${var.aws-access-key-id}\" | .cloud.aws.secretAccessKey=\"${var.aws-secret-access-key}\" | .cloud.aws.vpcID=\"\" | .nodes.defaultInstanceType=\"${var.default-instance-type}\" | .nodes.defaultVolumeSize=\"${var.default-volume-size}\" | .license.key=\"${var.license-key}\" | .license.id=\"${var.license-id}\" | .license.username=\"${var.license-username}\" | .license.password=\"${var.license-password}\"" /opt/milpa/etc/server.yml > /opt/milpa/etc/server.yml.new && mv /opt/milpa/etc/server.yml.new /opt/milpa/etc/server.yml
-sed -i 's#--milpa-endpoint 127.0.0.1:54555$#--milpa-endpoint 127.0.0.1:54555 --non-masquerade-cidr 172.20.0.0/16#' /etc/systemd/system/kiyot.service
+sed -i 's#--milpa-endpoint 127.0.0.1:54555$#--milpa-endpoint 127.0.0.1:54555 --service-cluster-ip-range 172.20.0.0/16#' /etc/systemd/system/kiyot.service
 sed -i 's#--config /opt/milpa/etc/server.yml$#--config /opt/milpa/etc/server.yml --delete-cluster-lock-file#' /etc/systemd/system/milpa.service
 mkdir -p /etc/systemd/system/kubelet.service.d/
 echo -e "[Service]\nStartLimitInterval=0\nStartLimitIntervalSec=0\nRestart=always\nRestartSec=5" > /etc/systemd/system/kubelet.service.d/override.conf
