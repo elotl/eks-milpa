5d22de1d8cc06f3a5d79e4f4eb831b157fd529f0
Vilmos Nebehaj <vilmos@elotl.co>
2019-06-21 16:10:22 -0700

    Remove superfluous SG rule

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -71,16 +71,6 @@ resource "aws_security_group" "demo-cluster" {
   }
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
-  description              = "Allow pods to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-cluster.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
-  to_port                  = 443
-  type                     = "ingress"
-}
-
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow access to the cluster API Server"
