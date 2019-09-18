8f1b31477a7e9c0ef204e6aa28c20a2782d1ff71
Vilmos Nebehaj <vilmos@elotl.co>
2019-09-18 16:04:28 -0700

    Inline cluster security group ingress

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -66,19 +66,17 @@ resource "aws_security_group" "clustersg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port         = 443
+    to_port           = 443
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    description       = "Allow access to the cluster API Server"
+  }
+
   tags = {
     Name = "eks-${var.cluster-name}"
   }
-}
-
-resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "Allow access to the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.clustersg.id
-  to_port           = 443
-  type              = "ingress"
 }
 
 resource "aws_eks_cluster" "cluster" {
