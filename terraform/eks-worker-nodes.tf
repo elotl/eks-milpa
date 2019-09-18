681ea1f286afd740ffd9465a6512ce681469620d
Vilmos Nebehaj <vilmos@elotl.co>
2019-09-18 13:27:19 -0700

    Add role label to regular workers

--- terraform/eks-worker-nodes.tf
+++ terraform/eks-worker-nodes.tf
@@ -182,7 +182,7 @@ locals {
   worker-userdata = <<USERDATA
 #!/bin/bash
 set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority.0.data}' '${var.cluster-name}'
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority.0.data}' --kubelet-extra-args '--node-labels=kubernetes.io/role=worker' '${var.cluster-name}'
 USERDATA
 
   milpa-worker-userdata = <<USERDATA
