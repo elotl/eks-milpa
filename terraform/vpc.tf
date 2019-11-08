51192a2ed1bd4006c530e5f07732fca9a60eef36
Vilmos Nebehaj <vilmos@elotl.co>
2019-11-08 12:48:44 -0800

    Enable internal dns hostnames in VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,6 +8,7 @@
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
 
   tags = {
     "Name"                                      = "eks-${var.cluster-name}"
