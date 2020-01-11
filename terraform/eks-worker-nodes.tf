742ffa4eaaccd7614ef9d7fb47a9abb25db0355e
Vilmos Nebehaj <vilmos@elotl.co>
2020-01-10 18:28:20 -0800

    Use client IP for ssh SG rule

--- terraform/eks-worker-nodes.tf
+++ terraform/eks-worker-nodes.tf
@@ -156,7 +156,7 @@ resource "aws_security_group" "worker-node" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [local.client-cidr]
   }
 
   tags = {
